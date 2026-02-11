15d727063ca24055dca767d002e7d723c6d5f006
HanSangHyun <xhaktm3344@gmail.com>
2026-02-11 17:24:23 +0900

    투두 리스트 UI 및 Storage 설치

--- TodosApp/App.tsx
+++ TodosApp/App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import {
   Pressable,
+  ScrollView,
   StyleSheet,
   Text,
   TextInput,
@@ -9,12 +10,13 @@ import {
 } from "react-native";
 import { theme } from "./colors";
 import { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
   const [todos, setTodos] = useState<{
-    [key: string]: { text: string; work: boolean };
+    [key: string]: { text: string; working: boolean };
   }>({});
 
   const onPressTravel = () => {
@@ -41,11 +43,11 @@ export default function App() {
 
     setTodos((prev) => ({
       ...prev,
-      [Date.now()]: { text, work: working },
+      [Date.now()]: { text, working },
     }));
     setText("");
   };
-  console.log(todos);
+  console.log(Object.keys(todos));
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -80,6 +82,16 @@ export default function App() {
         style={styles.input}
         placeholder={working ? "할일을 입력하세요" : "여행 계획을 입력하세요"}
       />
+      <ScrollView style={styles.todosContainer}>
+        {Object.keys(todos).map((todo) =>
+          todos[todo].working === working ? (
+            <View key={todo} style={styles.todo}>
+              <Text style={styles.todoText}>{todos[todo].text}</Text>
+            </View>
+          ) : null,
+        )}
+      </ScrollView>
+      <View style={styles.footer}></View>
     </View>
   );
 }
@@ -93,7 +105,7 @@ const styles = StyleSheet.create({
     // justifyContent: "center",
   },
   header: {
-    flex: 0.15,
+    flex: 0.2,
     // backgroundColor: "darkgray",
     flexDirection: "row",
     alignItems: "flex-end",
@@ -108,8 +120,28 @@ const styles = StyleSheet.create({
     color: "black",
     paddingHorizontal: 20,
     paddingVertical: 15,
+    marginVertical: 20,
     borderRadius: 30,
     marginTop: 20,
     fontSize: 16,
+  },
+  todosContainer: {
+    flex: 1,
+  },
+  todo: {
+    backgroundColor: theme.todoBg,
+    paddingHorizontal: 20,
+    paddingVertical: 25,
+    borderRadius: 20,
+    marginBottom: 10,
+  },
+  todoText: {
+    color: theme.white,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  footer: {
+    flex: 0.1,
+    backgroundColor: "teal",
   },
 });
--- TodosApp/colors.ts
+++ TodosApp/colors.ts
@@ -2,4 +2,5 @@ export const theme = {
   bg: "black",
   grey: "#3A3D40",
   white: "white",
+  todoBg: "#5C5C60",
 };
